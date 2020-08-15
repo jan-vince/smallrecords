0fbb076606c5439a7227de3198d456961dfe11fa
Roman Krylov <xtsidx@gmail.com>
2020-08-15 08:16:23 +0300

    Change content_blocks column in records table to longtext type

---
+++ updates/sr-update-10.php
@@ -0,0 +1,20 @@
+<?php namespace JanVince\SmallRecords\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class SmallRecordsTablesUpdate10 extends Migration
+{
+    public function up()
+    {
+
+        Schema::table('janvince_smallrecords_records', function($table) {
+            $table->longText('content_blocks')->change();
+        });
+    }
+
+    public function down()
+    {
+
+    }
+}
